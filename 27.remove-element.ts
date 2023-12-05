93dc506ba9766c0821eea1ac8d5e1755d65dc616
ysl0628 <yihsinlan@gmail.com>
2023-12-05 21:45:08 +0800

    Refactor removeElement function to use two pointers

--- 27.remove-element.ts
+++ 27.remove-element.ts
@@ -7,14 +7,19 @@
 // @lc code=start
 function removeElement(nums: number[], val: number): number {
   // nums = [3,2,2,3], val = 3
-  let i = 0 // 曼指針
+  let i = 0 // 慢指針
   // let j = 0; 快指針
   for (let j = 0; j < nums.length; j++) {
+    // 當快指針的值不等於目標值時，將快指針的值賦值給慢指針
+    // 並將慢指針向前移動一位
     if (nums[j] !== val) {
       nums[i] = nums[j]
       i++
     }
+    // 當快指針的值等於目標值時，不做任何操作，繼續向前移動
   }
+  // 最後返回慢指針的位置，即為新數組的長度
+
   return i
 }
 // @lc code=end
